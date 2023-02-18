86f578b5b0fbf931e7ee010253a804c6f9bf37c3
edkim3275 <edkim3275@gmail.com>
2023-02-18 21:06:26 +0900

    flutter for beginner first commit

---
+++ lib/home.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:learningdart/services/auth/auth_service.dart';
+import 'package:learningdart/views/login_view.dart';
+import 'package:learningdart/views/notes/notes_view.dart';
+import 'package:learningdart/views/verify_email_view.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: AuthService.firebase().initialize(),
+      builder: (context, snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.done:
+            final user = AuthService.firebase().currentUser;
+            // final emailVerified = user?.emailVerified ?? false;
+            if (user != null) {
+              if (user.isEmailVerified) {
+                return const NotesView();
+                // print('You are a verified user');
+              } else {
+                return const VerifyEmailView();
+              }
+            } else {
+              return const LoginView();
+            }
+          default:
+            return const CircularProgressIndicator();
+        }
+      },
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:learningdart/constants/routes.dart';
+import 'package:learningdart/home.dart';
+import 'package:learningdart/views/login_view.dart';
+import 'package:learningdart/views/notes/new_note_view.dart';
+import 'package:learningdart/views/notes/notes_view.dart';
+import 'package:learningdart/views/register_view.dart';
+import 'package:learningdart/views/verify_email_view.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MaterialApp(
+    title: 'MyApp',
+    home: HomePage(),
+    routes: {
+      // '/': (context) => const RegisterView(),
+      loginRoute: (context) => const LoginView(),
+      registerRoute: (context) => const RegisterView(),
+      notesRoute: (context) => const NotesView(),
+      verifyEmailRoute: (context) => const VerifyEmailView(),
+      newNoteRoute: (context) => const NewNoteView(),
+    },
+  ));
+}
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return const MaterialApp(
+//       title: 'MyApp',
+//       home: HomePage(),
+//     );
+//   }
+// }
